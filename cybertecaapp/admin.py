fd2df16b31427dd764205831887c12f51955f56f
Rogério <rogerio.pereira@ifto.edu.br>
2022-10-28 18:00:39 -0300

    versao3

--- cybertecaapp/admin.py
+++ cybertecaapp/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from cybertecaapp.models import Livros,Autores,Categorias
 
 # Register your models here.
+
+admin.site.register(Livros)
+admin.site.register(Autores)
+admin.site.register(Categorias)
+
--- cybertecaapp/models.py
+++ cybertecaapp/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
 # Create your models here.
+from django.db import models
+
+# Create your models here.
+
+
+
+class Categorias(models.Model):
+    categoria = models.CharField(max_length=255, null=True)
+
+
+class Autores(models.Model):
+    nome = models.CharField(max_length=255, null=True)
+
+
+
+class Livros(models.Model):
+    material = models.CharField(max_length=255)
+    chamada = models.CharField(max_length=255)
+    titulo = models.CharField(max_length=255)
+    ano = models.CharField (max_length=255)
+    data_cadastro = models.CharField (max_length=255)
+    imagem = models.ImageField(blank=True,null=True,)
+    categoriaid = models.ForeignKey(
+        'Categorias', on_delete=models.SET_NULL, null=True)
+    autoresid = models.ForeignKey(
+        'Autores', on_delete=models.SET_NULL, null=True)
